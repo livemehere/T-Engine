4086a30cc8a24f135462fe498a976efef89d76ba
livemehere <rhdxoals@gmail.com>
2026-06-03 05:13:17 +0900

    feat: add spdlog

--- Engine/include/EngineCommon.h
+++ Engine/include/EngineCommon.h
@@ -3,3 +3,18 @@
 #define GLFW_INCLUDE_NONE
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <spdlog/spdlog.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <functional>
+
+
+#define LOG_TRACE(...)    spdlog::trace(__VA_ARGS__)
+#define LOG_INFO(...)     spdlog::info(__VA_ARGS__)
+#define LOG_WARN(...)     spdlog::warn(__VA_ARGS__)
+#define LOG_ERROR(...)    spdlog::error(__VA_ARGS__)
+#define LOG_CRITICAL(...) spdlog::critical(__VA_ARGS__)
