1d15dd5d7f5a6c6bea3fb3efbecf072ea2b86ea1
livemehere <rhdxoals@gmail.com>
2026-06-03 23:18:40 +0900

    feat: simple log on info

--- Engine/include/EngineCommon.h
+++ Engine/include/EngineCommon.h
@@ -15,7 +15,7 @@
 
 
 #define LOG_TRACE(...)    SPDLOG_LOGGER_CALL(spdlog::default_logger_raw(), spdlog::level::trace, __VA_ARGS__)
-#define LOG_INFO(...)     SPDLOG_LOGGER_CALL(spdlog::default_logger_raw(), spdlog::level::info, __VA_ARGS__)
+#define LOG_INFO(...)     spdlog::info(__VA_ARGS__)
 #define LOG_WARN(...)     SPDLOG_LOGGER_CALL(spdlog::default_logger_raw(), spdlog::level::warn, __VA_ARGS__)
 #define LOG_ERROR(...)    SPDLOG_LOGGER_CALL(spdlog::default_logger_raw(), spdlog::level::err, __VA_ARGS__)
 #define LOG_CRITICAL(...) SPDLOG_LOGGER_CALL(spdlog::default_logger_raw(), spdlog::level::critical, __VA_ARGS__)
