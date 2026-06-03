222c7be7d32b2c86d876c76a17b5ca3923ea2386
livemehere <rhdxoals@gmail.com>
2026-06-04 04:17:15 +0900

    feat: IndexBuffer logging

--- Engine/include/IndexBuffer.h
+++ Engine/include/IndexBuffer.h
@@ -1,17 +1,19 @@
 #pragma once
 
+#include "EngineCommon.h"
+
 namespace Engine {
     class IndexBuffer {
-        public:
-            IndexBuffer(const unsigned int* indices, unsigned int count);
-            ~IndexBuffer();
+    public:
+        IndexBuffer(const unsigned int* indices, unsigned int count);
+        ~IndexBuffer();
 
-            void Bind() const;
-            void Unbind() const;
+        void Bind() const;
+        void Unbind() const;
 
-            unsigned int Getcount() const { return m_count; }
-        private:
-            unsigned int id;
-            unsigned int m_count;
+        unsigned int Getcount() const { return m_count; }
+    private:
+        unsigned int id = 0;
+        unsigned int m_count = 0;
     };
 }
--- Engine/src/IndexBuffer.cpp
+++ Engine/src/IndexBuffer.cpp
@@ -1,15 +1,19 @@
 #include "IndexBuffer.h"
 
 Engine::IndexBuffer::IndexBuffer(const unsigned int *indices, const unsigned int count) {
-    // save at GPU
     m_count = count;
     glGenBuffers(1, &id);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, id);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * count, indices, GL_STATIC_DRAW);
+    LOG_INFO("[IndexBuffer] ctor (id: {}, count: {})", id, m_count);
 }
 
 Engine::IndexBuffer::~IndexBuffer() {
-    glDeleteBuffers(1, &id);
+    LOG_INFO("[IndexBuffer] dtor (id: {}, count: {})", id, m_count);
+    if (id != 0) {
+        glDeleteBuffers(1, &id);
+        LOG_INFO("[IndexBuffer] GPU free (id: {})", id);
+    }
 }
 
 void Engine::IndexBuffer::Bind() const {
