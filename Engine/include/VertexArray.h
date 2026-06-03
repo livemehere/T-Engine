9f80df746beb20eb3d7c2839b8f720372b544d87
livemehere <rhdxoals@gmail.com>
2026-06-04 04:00:33 +0900

    wip: VertexArray

---
+++ Engine/include/VertexArray.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "EngineCommon.h"
+#include "IndexBuffer.h"
+#include "VertexBuffer.h"
+
+namespace Engine {
+    class VertexArray {
+    public:
+        VertexArray();
+        ~VertexArray();
+
+        void Bind() const;
+        void UnBind() const;
+
+        void AddVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer); // 이건왜 & 로받는가?
+        void SetIndexBuffer(const std::shared_ptr<IndexBuffer>& indexBuffer);
+
+    private:
+        unsigned int id;
+        // 이건왜 배열로 되어있을까.
+        std::vector<std::shared_ptr<VertexBuffer>> m_vertexBuffers;
+        std::shared_ptr<IndexBuffer> m_indexBuffer;
+    };
+}
