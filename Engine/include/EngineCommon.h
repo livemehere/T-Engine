149a29304bcdf74af9f26b513a61000c716b4c82
livemehere <rhdxoals@gmail.com>
2026-06-03 22:39:49 +0900

    impr: LOG_* show file, line info

--- Engine/include/EngineCommon.h
+++ Engine/include/EngineCommon.h
@@ -14,8 +14,8 @@
 #include <stdexcept>
 
 
-#define LOG_TRACE(...)    spdlog::trace(__VA_ARGS__)
-#define LOG_INFO(...)     spdlog::info(__VA_ARGS__)
-#define LOG_WARN(...)     spdlog::warn(__VA_ARGS__)
-#define LOG_ERROR(...)    spdlog::error(__VA_ARGS__)
-#define LOG_CRITICAL(...) spdlog::critical(__VA_ARGS__)
+#define LOG_TRACE(...)    SPDLOG_LOGGER_CALL(spdlog::default_logger_raw(), spdlog::level::trace, __VA_ARGS__)
+#define LOG_INFO(...)     SPDLOG_LOGGER_CALL(spdlog::default_logger_raw(), spdlog::level::info, __VA_ARGS__)
+#define LOG_WARN(...)     SPDLOG_LOGGER_CALL(spdlog::default_logger_raw(), spdlog::level::warn, __VA_ARGS__)
+#define LOG_ERROR(...)    SPDLOG_LOGGER_CALL(spdlog::default_logger_raw(), spdlog::level::err, __VA_ARGS__)
+#define LOG_CRITICAL(...) SPDLOG_LOGGER_CALL(spdlog::default_logger_raw(), spdlog::level::critical, __VA_ARGS__)
