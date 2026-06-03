a2fdf7286cbb1574a7709a31d96973aae9513950
livemehere <rhdxoals@gmail.com>
2026-06-03 23:18:45 +0900

    feat: EBO

--- App/src/main.cpp
+++ App/src/main.cpp
@@ -86,13 +86,25 @@ public:
             0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f// right / b
         };
 
+        std::vector<unsigned int> indices = {
+            0,1,2
+        };
+
+        // VAO
+        glGenVertexArrays(1, &VAO);
+        glBindVertexArray(VAO);
+
+        // VBO
         GLuint VBO;
         glGenBuffers(1, &VBO);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(float) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
 
-        glGenVertexArrays(1, &VAO);
-        glBindVertexArray(VAO);
+        // EBO
+        GLuint EBO;
+        glGenBuffers(1, &EBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER,EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indices.size(), indices.data(), GL_STATIC_DRAW);
 
         // xyz
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
@@ -101,15 +113,19 @@ public:
         // rgba
         glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(3*sizeof(float)));
         glEnableVertexAttribArray(1);
+
+
     }
 
     void OnUpdate(float dt) override {
     }
 
     void OnRender() override {
+        glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES,3, GL_UNSIGNED_INT, 0);
     }
 };
 
